5b061f0a75acb864d22cdcefe60edec51f6ad6bc
Soomin <Soomin@Soomin-MacBook-Pro.local>
2017-03-25 16:09:00 +0900

    First commit

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 10,
+    loop: true
+});
